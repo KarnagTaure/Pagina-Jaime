4e9ef3b27774f344e96f7cc63845880ea10594ac
Karnag <albertolopma@gmail.com>
2023-12-09 02:06:34 +0100

    1

--- situaciones.js
+++ situaciones.js
@@ -55,29 +55,31 @@ function cambiarImagenAlClic() {
   // Busca el div anteriormente clicado por su clase 'clicado'
   const anteriorBoton = document.querySelector('.clicado');
 
+//Mira si hay anteriror clicado y si el clicado no es el que ahora clicamos
   if (anteriorBoton && anteriorBoton !== this) {
     // Si hay un div anteriormente clicado y no es el mismo, restaura su imagen y clase
     const imgCambiada = anteriorBoton.style.backgroundImage.replace('_on.png', '.png');
-    anteriorBoton.style.backgroundImage = imgCambiada;
-    anteriorBoton.classList.remove('clicado');
-    anteriorBoton.classList.add('situacion');
-    anteriorBoton.dataset.clicado = 'false';
+    anteriorBoton.style.backgroundImage = imgCambiada;//cambia la imagen
+    anteriorBoton.classList.remove('clicado');//Quita la clase
+    anteriorBoton.classList.add('situacion');//Añade la clase
+    anteriorBoton.dataset.clicado = 'false';//cambia el booleano clicado
   }
 
   // Obtiene el valor del atributo data-clicado
   const clicado = this.classList.contains('clicado');
 
   if (!clicado) {
-    // Obtiene la URL original desde un atributo personalizado
+    // Obtiene la URL original desde un atributo personalizado del elemento clicado
     const imagenOriginal = this.style.backgroundImage;
 
-    // Construye la nueva URL con "_on" antes de ".png"
-    const nuevaImg = imagenOriginal.replace('.png', '.png');
+    // Como imagenOriginal al clicarla con el raton y 
+    //este al estar encima cambia la imagen no hace falta cambiar la terminacion de la url
+    const nuevaImg = imagenOriginal;
 
     // Establece la nueva imagen de fondo y clase para el div actual
     this.style.backgroundImage = nuevaImg;
-    // this.classList.remove('situacion');
-     this.classList.add('clicado');
+    
+     this.classList.add('clicado');//Cambia el nombre de la clase
    
 
     // Establece el atributo data-clicado a trues
